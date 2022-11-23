c5f86609b1137d1bbba20cdefcfcf088b96d1031
Lucas <lucasmeinen@yahoo.com.br>
2022-11-23 17:58:01 -0300

    :goal_net: handling errors

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app =  express();
+const morgan = require('morgan');
 
-const rotasProdutos = require('./routes/produtos');
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos')
 
-app.use('/produtos', rotasProdutos);
+app.use(morgan('dev'));
 
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
+// Quando não encontra a rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.mensagem
+        }
+    })
+})
 
 module.exports = app;
