874ea0b99be61dd1ffdae35cb75cb3cb9e5aa6f7
Lucas <lucasmeinen@yahoo.com.br>
2022-11-23 16:05:57 -0300

    :bulb:docs: add comments in crud

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Lista todos os produtos
 router.get('/', (req, res, next) => {
     res.status(200).send({
         mensagem: 'Usando o Get dentro da rota de produtos'
     })
 });
 
+// Insere um produto
 router.post('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Usando o Post na rota de produtos'
     })
-})
+});
 
+// Lista um produto pelo id específico
 router.get('/:id_produto', (req, res, next) => {
     const id = req.params.id_produto
-    res.status(200).send({
-        mensagem: 'Usando o Get de um produto exclusivo',
-        id:id
+
+    if(id === especial){
+        res.status(200).send({
+            mensagem: 'Você descobriu o id especial',
+            id:id
+        })
+    }else{
+        res.status(200).send({
+            mensagem: 'Você passou um id'
+        })
+    }
+});
+
+// Altera um produto
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o Patch dentro da rota de produtos'
+    })
+});
+
+// Deleta um produto
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o Delete dentro da rota de produtos'
     })
 })
 
