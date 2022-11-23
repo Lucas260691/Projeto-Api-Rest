81421130c4a29cea4a08da658d3cab0099167fd6
Lucas <lucasmeinen@yahoo.com.br>
2022-11-23 16:15:45 -0300

    :sparkles: feat: Create Routes orders

---
+++ routes/pedidos.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Lista todos os pedidos
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna os pedidos'
+    })
+});
+
+// Insere um pedido
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Insere um pedido'
+    })
+});
+
+// Lista dados de um pedido
+router.get('/:id_pedido', (req, res, next) => {
+    const id = req.params.id_pedido
+    res.status(200).send({
+        mensagem: 'Detalhes de um pedido',
+        id_pedido: id
+    })   
+});
+
+// Deleta um pedido
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Pedido excluído'
+    })
+});
+
+module.exports = router;
