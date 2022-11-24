c8b7d6af14317efa39c74ae648d038440711c35d
Lucas <lucasmeinen@yahoo.com.br>
2022-11-24 01:14:03 -0300

    :wrench:chore: config cors in app

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
 app.use(bodyParser.json()); // json de entrada no body
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Content-Type',
+        'Origin',
+        'X-Requested-With',
+        'Accept',
+        'Authorization'
+    );
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
+})
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
