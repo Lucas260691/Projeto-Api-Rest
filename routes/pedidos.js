478153294df4eac0d4437a14b2c9d9d2e2edc42a
Lucas <lucasmeinen@yahoo.com.br>
2022-11-27 19:02:36 -0300

    :sparkles: feat: get by id in orders

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -64,12 +64,37 @@ router.post('/', (req, res, next) => {
 });
 
 // Lista dados de um pedido
-router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido
-    res.status(200).send({
-        mensagem: 'Detalhes de um pedido',
-        id_pedido: id
-    })   
+router.get('/:id_pedidos', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error) { return res.status(500).send({ error: error})}
+        conn.query(
+            'SELECT * FROM pedidos WHERE id_pedidos = ?;',
+            [req.params.id_pedidos],
+            (error, result, fields) => {
+                if(error) { return res.status(500).send({ error: error})}
+                
+                if(result.length == 0) {
+                    return res.status(404).send({
+                        mensagem:'Não foi encontrado pedido com este ID'
+                    })
+                }
+                
+                const response = {
+                    pedido: {
+                        id_pedidos: result[0].id_pedidos,
+                        id_produtos: result[0].id_produtos,
+                        quantidade: result[0].quantidade,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna todos os pedidos',
+                            url: 'http://localhost:3000/pedidos'
+                        }
+                    }
+                }
+                return res.status(200).send(response);
+            }
+        )
+    })
 });
 
 // Deleta um pedido
