1819d2f808a3fcbbcbc70e63f296ca721e4eb429
Lucas <lucasmeinen@yahoo.com.br>
2022-11-27 15:38:15 -0300

    :sparkles: feat: get in orders

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+const mysql = require('../mysql');
 
 // Lista todos os pedidos
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Retorna os pedidos'
-    })
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            'SELECT * FROM pedidos;',
+            (error, result, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    quantidade: result.length,
+                    pedidos: result.map(pedido => {
+                        return {
+                            id_pedidos: pedido.id_pedidos,
+                            id_produtos: pedido.id_produtos,
+                            quantidade: pedido.quantidade,
+                            request: {
+                                tipo: 'GET',
+                                descrição: 'Retorna os detalhes de um pedido específico',
+                                url: 'http://localhost:3000/pedidos/' + pedido.id_pedidos
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response);
+        }
+     )
+  })
 });
 
 // Insere um pedido
